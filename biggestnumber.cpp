ae314fe804310ee8e132bd54d56bb806b76e4fd9
afajhan1 <afajhan@mail.csuchico.edu>
2018-02-06 20:14:49 -0800

    first step

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,16 +1,21 @@
-//Author:
+//Author:ahmad fajhan
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+ int value1,value2;
 
-  cout<<"Of those two numbers, the biggest is: ";
+  cout<<"Please enter a whole number:\n";
+
+cin>> value1; 
+
+cout<<"Please enter another whole number:\n";
+
+cin>>value2;
+
+cout<<"Of those two numbers, the biggest is: ";
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
